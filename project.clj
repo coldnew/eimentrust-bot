1e8731910e539a593d16a177150cbccc63667132
Yen-Chin Lee <coldnew.tw@gmail.com>
2018-05-14 16:37:37 +0800

    first commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject eimentrust-bot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/coldnew/eimentrust-bot"
+  :license {:name "GNU Affero General Public License 3.0 (AGPL-3.0)"
+            :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
---
+++ test/eimentrust_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns eimentrust-bot.core-test
+  (:require [clojure.test :refer :all]
+            [eimentrust-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
